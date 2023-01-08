049e3e843ca0073f2bc7f5f66f0b025ba1da1872
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2023-01-08 13:47:21 +1100

    github actions

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,28 @@
 export default {
   title: 'Library-free state',
-  description: 'Just playing around.'
+  description: 'Just playing around.',
+  base: 'lib-free-state',
+  themeConfig: {
+    siteTitle: 'Library-free state',
+    nav: [,
+      { text: 'Guide', link: '/guide' },
+      {
+        text: 'Dropdown Menu',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' }
+        ]
+      }
+    ],
+    sidebar: [
+      {
+        text: 'Introduction',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ],
+      }
+    ]
+  }
 }
